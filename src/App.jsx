ca8fbaa0756fbb450bd2a6bda9a8a66f3a7ff6e8
Salman <salmannazeer477@gmail.com>
2026-03-24 15:15:40 +0530

    Started creating  handleSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Calendar from './Calendar'
+import { useState } from 'react'
 
 export default function App() {
+  const [text, setText] = useState("")
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(text)
+  }
+
   return (
     <>
-      <form>
-        <textarea rows={10} cols={36} name="text" id="text"></textarea>
+      <form onSubmit={handleSubmit}>
+        <textarea rows={10} cols={36} name="text" id="text" onChange={e => setText(e.target.value)}></textarea>
         <button>Submit</button>
       </form>
       <Calendar />
